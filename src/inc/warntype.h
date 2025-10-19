de537b3cf8acd89121c82d3eafe0257456a47835
ngtv <tuanvy860@gmail.com>
2025-10-19 16:10:42 +0700

    feat (/): add warntype.h to define clarity types

---
+++ src/inc/warntype.h
@@ -0,0 +1,12 @@
+#pragma once
+
+/*
+ * This header contains type aliases and macros 
+ * which improve the clarity about what the type is, how it should be use
+ * whether it is allocated or freeable or just a pointer
+ * to an offset of another str */
+
+// str is allocated by malloc, could be free
+typedef char* _sefStrAlloc;
+// str is allocated on the heap but is not the root pointer, cannot be freed
+typedef char* _sefStrOffset;
